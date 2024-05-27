046482a5585e583ed576eeceb57966c939f33f4f
SANCHES_VARELA Nicolas <nicolas.sanches_varela@macsf.fr>
2024-05-27 11:35:34 +0200

    read HTTP request

--- src/main/java/org/apache/maven/archetypes/SimpleHTTPServer.java
+++ src/main/java/org/apache/maven/archetypes/SimpleHTTPServer.java
@@ -1,15 +1,32 @@
 package org.apache.maven.archetypes;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
 
+/** 
+ * Java program to create a simple HTTP Server to demonstrate how to use
+ * ServerSocket and Socket class.
+ * @author NsanVarela
+ */
 public class SimpleHTTPServer 
 {
-    public static void main( String[] args ) throws Exception
-    {
-        final ServerSocket server = new ServerSocket(8080);
+    public static void main( String[] args ) throws Exception {
+        ServerSocket server = new ServerSocket(8080);
     	System.out.println( "Listening for connection on port 8080 ..." );
     	while (true) {
-    		// spin forever
+    		Socket clientSocket = server.accept();
+    		
+    		// 1. Read HTTP request from the client socket
+    		InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+    		BufferedReader reader = new BufferedReader(isr);
+    		String line = reader.readLine();
+    		while (!line.isEmpty()) {
+    			System.out.println(line);
+    			line = reader.readLine();
+    		}
     	}
     }
 }
